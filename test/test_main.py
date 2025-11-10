54cb6cc8e20f88843dd2a9566973b6a9ce0ff65a
nange <cctonange@gmail.com>
2025-11-10 18:04:49 +0800

    fix lint

--- test/test_main.py
+++ test/test_main.py
@@ -1,9 +1,10 @@
 import unittest
 
+
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
         self.assertEqual("hello world", "hello world")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()

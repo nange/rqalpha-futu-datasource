e172ca6fbf1ed84d81421bed32487ecc565235f0
nange <cctonange@gmail.com>
2025-11-25 17:14:43 +0800

    add handle_bar_1m test

--- tests/test_rqalpha_example.py
+++ tests/test_rqalpha_example.py
@@ -65,6 +65,48 @@ def handle_bar(context, bar_dict):
     assert float(obj.total_turnover) == 14544469827.0
 
 
+def handle_bar_1m(context, bar_dict):
+    now = context.now
+    if now.strftime("%Y-%m-%d %H:%M:%S") != "2024-11-01 09:31:00":
+        return
+
+    obj = bar_dict["000001.XSHE"]
+    assert obj.datetime.strftime("%Y-%m-%d %H:%M:%S") == "2024-11-01 09:31:00"
+    assert float(obj.open) == 10.772
+    assert float(obj.high) == 10.792
+    assert float(obj.low) == 10.772
+    assert float(obj.close) == 10.792
+    assert float(obj.volume) == 2425320.0
+    assert float(obj.total_turnover) == 27597021.81
+
+    obj = bar_dict["600000.XSHG"]
+    assert obj.datetime.strftime("%Y-%m-%d %H:%M:%S") == "2024-11-01 09:31:00"
+    assert float(obj.open) == 9.45
+    assert float(obj.high) == 9.48
+    assert float(obj.low) == 9.43
+    assert float(obj.close) == 9.47
+    assert float(obj.volume) == 1693988.0
+    assert float(obj.total_turnover) == 16708452.0
+
+    obj = bar_dict["00700.XHKG"]
+    assert obj.datetime.strftime("%Y-%m-%d %H:%M:%S") == "2024-11-01 09:31:00"
+    assert float(obj.open) == 401.5
+    assert float(obj.high) == 406.3
+    assert float(obj.low) == 401.5
+    assert float(obj.close) == 406.1
+    assert float(obj.volume) == 285100.0
+    assert float(obj.total_turnover) == 116670900.0
+
+    obj = bar_dict["AAPL.XNAS"]
+    assert obj.datetime.strftime("%Y-%m-%d %H:%M:%S") == "2024-11-01 09:31:00"
+    assert float(obj.open) == 219.728380867
+    assert float(obj.high) == 220.58853994
+    assert float(obj.low) == 219.037270399
+    assert float(obj.close) == 220.190778519
+    assert float(obj.volume) == 1869295.0
+    assert float(obj.total_turnover) == 412994919.211
+
+
 def test_run_with_futu_datasource():
     config = {
         "base": {
@@ -87,3 +129,26 @@ def test_run_with_futu_datasource():
         },
     }
     run_func(init=init, handle_bar=handle_bar, config=config)
+
+
+def test_run_with_futu_datasource_1m():
+    config = {
+        "base": {
+            "start_date": "2024-11-01",
+            "end_date": "2024-11-01",
+            "accounts": {"stock": 100000},
+            "frequency": "1m",
+        },
+        "extra": {
+            "log_level": "info",
+        },
+        "mod": {
+            "futu_ds": {
+                "enabled": True,
+                "lib": "rqalpha_futu_datasource.mod_futu_ds",
+                "futu_data_path": os.path.abspath("tests/data"),
+            },
+            "sys_analyser": {},
+        },
+    }
+    run_func(init=init, handle_bar=handle_bar_1m, config=config)
